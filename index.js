f90b53a9a417459579e06063a1823d30aad1ff64
Apanovich, Alexander <aapanovich@qulix.com>
2020-03-23 01:19:18 +0300

    Port fix

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-var TelegramBot = require('node-telegram-bot-api');
-var usersService = require('./usersService')
-var token = '1123925771:AAEaRMmo9WWg7FOlz1w5Wgz7YDFXQFil8d8';
-var bot = new TelegramBot(token, { polling: true });
+var http = require('http')
 
-bot.onText(/start/, function (msg) {
-    let user = msg.from.id;
-    usersService.addUser(user);
-})
+http.createServer(function (request, response) {
+    var TelegramBot = require('node-telegram-bot-api');
+    var usersService = require('./usersService')
+    var token = '1123925771:AAEaRMmo9WWg7FOlz1w5Wgz7YDFXQFil8d8';
+    var bot = new TelegramBot(token, { polling: true });
+    ServiceWorkerRegistration
+    bot.onText(/start/, function (msg) {
+        let user = msg.from.id;
+        usersService.addUser(user);
+    })
 
-bot.onText(/quit/, function (msg) {
-    let user = msg.from.id;
-    usersService.removeUser(user);
-})
+    bot.onText(/quit/, function (msg) {
+        let user = msg.from.id;
+        usersService.removeUser(user);
+    })
 
-setInterval(function () {
-    let users = usersService.getUsers();
-    console.log(users)
-    for (let i = 0; i < users.length; i++) {
-        bot.sendMessage(users[i], 'хай');
-    }
-}, 1000);
+    setInterval(function () {
+        let users = usersService.getUsers();
+        for (let i = 0; i < users.length; i++) {
+            bot.sendMessage(users[i], 'хай');
+        }
+    }, 1000);
+}).listen(process.env.PORT)
