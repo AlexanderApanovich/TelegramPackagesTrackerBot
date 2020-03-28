76d07c80043e93aa72bc3deed7e4ef09eaba5d3c
Apanovich, Alexander <aapanovich@qulix.com>
2020-03-29 01:35:52 +0300

    Send info

--- index.js
+++ index.js
@@ -1,36 +1,69 @@
-var http = require("http");
-var TelegramBot = require('node-telegram-bot-api');
-var usersService = require('./usersService')
-var token = '1123925771:AAEaRMmo9WWg7FOlz1w5Wgz7YDFXQFil8d8';
-var bot = new TelegramBot(token, { polling: true });
-var me = 725757794;
+(async () => {
 
-bot.onText(/start/, function (msg) {
-    let user = msg.from.id;
-    usersService.addUser(user);
-})
+    var TelegramBot = require('node-telegram-bot-api');
+    var usersService = require('./usersService')
+    var parseService = require('./parseService')
+    var token = '1123925771:AAEaRMmo9WWg7FOlz1w5Wgz7YDFXQFil8d8';
+    var bot = new TelegramBot(token, { polling: true });
+    var me = 725757794;
 
-bot.onText(/quit/, function (msg) {
-    let user = msg.from.id;
-    usersService.removeUser(user);
-})
+    initBot();
 
-process.on('uncaughtException', function (err) {
-    bot.sendMessage(me, err);    
-})
+    process.on('uncaughtException', function (err) {
+        bot.sendMessage(me, err.message);
+    })
 
-setInterval(function () {
 
-    let users = usersService.getUsers();
-    for (let i = 0; i < users.length; i++) {
-        bot.sendMessage(users[i], 'хай');
+    // setInterval(async () => {
+    // let users = usersService.getUsers();
+    // }, 300000);
+
+
+    function sendInfo(user) {
+        parseService.parse().then(function (value) {
+
+            // users.forEach(user => {
+                let message = getMessage(value);
+
+                bot.sendMessage(user, message);
+            })
+        // }, function (reason) { });
     }
-    // parse();
-
-}, 1000);//300000);
 
 
 
 
-// http.createServer(function (request, response) {
-// }).listen(process.env.PORT)
+
+
+    function getMessage(packages) {
+        let message = "";
+
+        for (let i = 0; i < packages.length; i++) {
+            let package = packages[i];
+
+            let packageMessage = `Посылка "${package.name}": \nпоследний трекинг ${package.statuses[0].date
+                .toLocaleDateString('ru-RU')}\n${package.statuses[0].text}\n===============================\n`;
+
+            message = message + packageMessage;
+        }
+
+        return message;
+    }
+
+    function initBot() {
+        bot.onText(/start/, function (msg) {
+            let user = msg.from.id;
+            usersService.addUser(user);
+        })
+
+        bot.onText(/quit/, function (msg) {
+            let user = msg.from.id;
+            usersService.removeUser(user);
+        })
+
+        bot.onText(/info/, function (msg) {
+            let user = msg.from.id;
+            sendInfo(user);
+        })
+    }
+})();
---
+++ parseService.js
@@ -0,0 +1,85 @@
+const request = require("request");
+const cheerio = require("cheerio");
+const fetch = require("node-fetch");
+
+var urls = [
+    {
+        url: "https://gdeposylka.ru/courier/omniva/tracking/RO427576146EE",
+        name: "Raspberry"
+    },
+    {
+        url: "https://gdeposylka.ru/courier/omniva/tracking/RO429132763EE",
+        name: "SD карта"
+    },
+    {
+        url: "https://gdeposylka.ru/courier/china-ems/tracking/LA099161403CN",
+        name: "Блок питания"
+    },
+    {
+        url: "https://gdeposylka.ru/courier/omniva/tracking/SB082733098EE",
+        name: "RCA кабель 1,5 м"
+    },
+    {
+        url: "https://gdeposylka.ru/courier/china-ems/tracking/LA096961183CN",
+        name: "Корпус"
+    }
+]
+
+function parseBody(body) {
+
+    var statuses = [];
+    var $ = cheerio.load(body, { decodeEntities: false });
+    var packageStatusSections = $(".info.status-iconed");
+
+    $(packageStatusSections).each(function (index, value) {
+        var statusSection = $(value);
+
+        var statusValue = statusSection.children(".checkpoint-status").first().html();
+        var trimmedStatusValue = statusValue.trim();
+
+        var statusDate = statusSection.children(".datetime2").attr('datetime');
+        var parsedStatusDate = new Date(Date.parse(statusDate));
+
+        var status = { text: trimmedStatusValue, date: parsedStatusDate };
+
+        statuses.push(status);
+    });
+
+    return statuses;
+}
+
+async function parse() {
+    return new Promise(async (resolve, reject) => {
+
+        var packagesInfo = [];
+
+        for (const element of urls) {
+
+            var body = await fetch(element.url)
+                .then(function (response) {
+                    return response.text();
+                });
+
+            var statuses = parseBody(body);
+            
+            packagesInfo.push(
+                {
+                    url: element.url,
+                    name: element.name,
+                    statuses: statuses
+                }
+            );
+        }
+
+        if (packagesInfo.length > 0) {
+            resolve(packagesInfo);
+        }
+        else {
+            reject();
+        }
+    });
+}
+
+module.exports = {
+    parse: parse
+}
