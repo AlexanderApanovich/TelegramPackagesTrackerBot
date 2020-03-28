70cefddfe1d98f1e948afc0d8f63fe0fa293bbe8
Apanovich, Alexander <aapanovich@qulix.com>
2020-03-29 02:18:14 +0300

    Return server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 (async () => {
 
+    var express = require('express');
+    var app = express();
     var TelegramBot = require('node-telegram-bot-api');
     var usersService = require('./usersService')
     var parseService = require('./parseService')
@@ -7,57 +9,56 @@
     var bot = new TelegramBot(token, { polling: true });
     var me = 725757794;
 
-    bot.onText(/start/, function (msg) {
-        let user = msg.from.id;
-        usersService.addUser(user);
-    })
+    app.listen(process.env.PORT, function () {
 
-    bot.onText(/quit/, function (msg) {
-        let user = msg.from.id;
-        usersService.removeUser(user);
-    })
-
-    bot.onText(/info/, function (msg) {
-        let user = msg.from.id;
-        sendInfo(user);
-    })
-
-    process.on('uncaughtException', function (err) {
-        bot.sendMessage(me, err.message);
-    })
-
-    setInterval(() => {
-        let users = usersService.getUsers();
-    }, 5000);
-
-    function sendInfo(user) {
-        parseService.parse().then(function (value) {
-
-            // users.forEach(user => {
-            let message = getMessage(value);
-
-            bot.sendMessage(user, message);
+        bot.onText(/start/, function (msg) {
+            let user = msg.from.id;
+            usersService.addUser(user);
         })
-        // }, function (reason) { });
-    }
 
+        bot.onText(/quit/, function (msg) {
+            let user = msg.from.id;
+            usersService.removeUser(user);
+        })
 
+        bot.onText(/info/, function (msg) {
+            let user = msg.from.id;
+            sendInfo(user);
+        })
 
+        process.on('uncaughtException', function (err) {
+            bot.sendMessage(me, err.message);
+        })
 
+        function sendInfo(user) {
+            parseService.parse().then(function (value) {
 
+                // users.forEach(user => {
+                let message = getMessage(value);
 
-    function getMessage(packages) {
-        let message = "";
-
-        for (let i = 0; i < packages.length; i++) {
-            let package = packages[i];
-
-            let packageMessage = `Посылка "${package.name}": \nпоследний трекинг ${package.statuses[0].date
-                .toLocaleDateString('ru-RU')}\n${package.statuses[0].text}\n===============================\n`;
-
-            message = message + packageMessage;
+                bot.sendMessage(user, message);
+            })
+            // }, function (reason) { });
         }
 
-        return message;
-    }    
+        function getMessage(packages) {
+            let message = "";
+
+            for (let i = 0; i < packages.length; i++) {
+                let package = packages[i];
+
+                let packageMessage = `Посылка "${package.name}": \nпоследний трекинг ${package.statuses[0].date
+                    .toLocaleDateString('ru-RU')}\n${package.statuses[0].text}\n===============================\n`;
+
+                message = message + packageMessage;
+            }
+
+            return message;
+        }
+        
+        setInterval(() => {
+            let users = usersService.getUsers();
+        }, 5000);
+    });
+
 })();
